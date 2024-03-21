61ae0b9f9f2f9a0707af0a66ec15684880388aba
Nitin Nair <nitin7nair@gmail.com>
2024-03-21 10:14:14 -0400

    added input option for adc correction mode

--- src/EmotiBitSerial.h
+++ src/EmotiBitSerial.h
@@ -20,6 +20,7 @@ public:
     */
     struct Inputs
     {
+        static const char ADC_CORRECTION_MODE = 'A';  ///< enter ADC correction mode. Backward compatibility to emotibit v2/v3 
         static const char RESET = 'R';  ///< resets MCU
         static const char CONFIG_UPDATE = 'C';  ///< Enters config file update mode.
         static const char DEBUG_MODE = 'D';   ///< Enters Debug mode
